0d96dd422659e459d05ca079f94500da4a7108d2
Sam <s@sammagee.me>
2021-01-17 10:00:51 -0600

    add apple icons/favicon

--- resources/js/Utils/theme.js
+++ resources/js/Utils/theme.js
@@ -2,7 +2,9 @@ export function updateTheme(theme) {
   const root = document.querySelector('html')
   if (theme === 'light') {
     root.classList.remove('dark')
+    document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').setAttribute('content', 'default')
   } else {
     root.classList.add('dark')
+    document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').setAttribute('content', 'black-translucent')
   }
 }
