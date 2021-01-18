8c70087fd7e68b848ac97f3c5fd72a575276848e
Sam <s@sammagee.me>
2021-01-18 15:42:21 -0600

    update default content to be "normal" python code

--- resources/js/Config/Content.js
+++ resources/js/Config/Content.js
@@ -1,37 +1,23 @@
-export default String.raw`import turtle
+export default String.raw`"""
+Welcome to Py!
+---
+In this environment, you can create and share
+your Python code with the world.
+"""
 
-screen = turtle.Screen()
-screen.setup(500, 500)
+from random import randint
 
-t = turtle.Turtle()
-t.shape("triangle")
-t.penup()
-t.goto(45, 90)
-t.pendown()
+# Produces a list of random numbers and their squares.
+# Random numbers start at min and end at max.
+def get_random_squares(min, max):
+  n = randint(min, max)
+  nums = [str(num) for num in range(min, n + 1)]
+  squares = [str(num ** 2) for num in range(min, n + 1)]
+  row_format ="\t{:<3}" * 3
 
-for i in range(8):
-    t.right(45)
-    t.forward(90)
-    t.stamp()
+  print(f"Squares 1..{n}:\n")
+  for num, square in zip(nums, squares):
+    print(row_format.format(num, '=>', square))
 
-screen.exitonclick()
+get_random_squares(min=1, max=10)
 `
-// """
-// Welcome to Py!
-// """
-
-// from random import randint
-
-// # Produces a list of random numbers and their squares.
-// # Random numbers start at min and end at max.
-// def get_random_squares(min, max):
-//   n = randint(min, max)
-//   nums = [str(num) for num in range(min, n + 1)]
-//   squares = [str(num ** 2) for num in range(min, n + 1)]
-//   row_format ="\t{:<3}" * 3
-
-//   print(f"Squares 1..{n}:\n")
-//   for num, square in zip(nums, squares):
-//     print(row_format.format(num, '=>', square))
-
-// get_random_squares(min=1, max=10)
