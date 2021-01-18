35a57d9ecf21356ad80afd01a6ed19a87b283dc3
Sam <s@sammagee.me>
2021-01-18 15:43:04 -0600

    align header for auth cards to be more like normal app on small screens

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="flex flex-col items-center min-h-screen py-6 bg-white sm:justify-center sm:pt-0 dark:bg-gray-900">
-        <div class="flex w-full px-4 sm:max-w-sm">
-            <slot name="logo" />
+    <div class="flex flex-col items-center min-h-screen py-6 bg-white pt-safe-top sm:justify-center sm:pt-0 dark:bg-gray-900">
+        <div class="flex items-center w-full px-2 sm:w-auto">
+            <div class="flex w-full py-1 mt-px pl-safe-left pr-safe-right sm:px-4 sm:max-w-sm">
+                <slot name="logo" />
+            </div>
         </div>
 
-        <div class="w-full mt-2 overflow-hidden bg-white border border-gray-100 shadow-lg dark:border-gray-800 dark:bg-gray-900 sm:max-w-sm sm:rounded-lg">
+        <div class="w-full overflow-hidden bg-white border-t border-b border-gray-100 sm:mt-2 sm:border-r sm:border-l dark:border-gray-800 dark:bg-gray-900 sm:max-w-sm sm:rounded-lg">
             <slot />
         </div>
     </div>
