ee2918311553f8b1df514f7f8dacac3ed10b3fec
Sam <s@sammagee.me>
2021-01-22 09:31:07 -0600

    clean up and add "window" ui to canvas for turtle

--- resources/js/Components/Output.vue
+++ resources/js/Components/Output.vue
@@ -16,10 +16,9 @@
 
         <transition leave-active-class="duration-200">
             <div
-                v-show="showCanvas"
+                v-show="show"
                 class="absolute inset-0 z-20 flex items-center justify-center flex-1 overflow-hidden"
                 :class="{ 'ml-safe-left': settings.layout === 'horizontal' }"
-                @click="closeCanvas"
             >
                 <transition
                     enter-active-class="duration-300 ease-out"
@@ -30,7 +29,7 @@
                     leave-to-class="opacity-0"
                 >
                     <div
-                        v-show="showCanvas"
+                        v-show="show"
                         class="absolute inset-0 flex items-center justify-center flex-1 transition-opacity bg-gray-100 bg-opacity-25 dark:bg-black dark:bg-opacity-25 backdrop-filter-blur mr-safe-right focus:outline-none"
                     />
                 </transition>
@@ -44,10 +43,51 @@
                     leave-to-class="translate-y-4 opacity-0 sm:translate-y-0 sm:scale-95"
                 >
                     <div
-                        v-show="showCanvas"
-                        id="canvas"
-                        class="overflow-auto transition-all transform bg-white rounded-lg shadow-xl focus:outline-none"
-                    />
+                        v-show="show"
+                        class="overflow-hidden bg-black shadow-xl"
+                        :class="{
+                            'rounded-lg': isMac,
+                            'rounded-none': !isMac,
+                        }"
+                    >
+                        <header
+                            class="flex items-center bg-black"
+                            :class="{
+                                'justify-start p-4': isMac,
+                                'justify-end': !isMac,
+                            }"
+                        >
+                            <button
+                                class="inline-flex items-center justify-center group focus:outline-none focus:ring-0 h-"
+                                :class="{
+                                    'w-3 h-3 bg-red-400 rounded-full focus:bg-red-300': isMac,
+                                    'w-14 h-11 hover:bg-red-400 focus:bg-red-400 transition ease-in-out duration-150': !isMac,
+                                }"
+                                @click="show = false"
+                            >
+                                <svg
+                                    :class="{
+                                        'w-2 h-2 text-red-900 hidden group-focus:inline group-hover:inline': isMac,
+                                        'w-6 h-6 text-gray-500 group-focus:text-white group-hover:text-white transition ease-in-out duration-150': !isMac,
+                                    }"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </header>
+
+                        <div class="overflow-auto transition-all transform bg-white focus:outline-none">
+                            <div id="canvas" />
+
+                            <div
+                                class="absolute inset-0"
+                                @click="close"
+                            />
+                        </div>
+                    </div>
                 </transition>
             </div>
         </transition>
@@ -60,15 +100,17 @@
     export default {
         setup() {
             const settings = inject('settings')
+            const isMac = inject('isMac')
             const output = inject('output')
-            const showCanvas = inject('showCanvas')
-            const closeCanvas = inject('closeCanvas')
+            const show = inject('showCanvas')
+            const close = inject('closeCanvas')
 
             return {
-                settings,
+                close,
+                isMac,
                 output,
-                showCanvas,
-                closeCanvas,
+                settings,
+                show,
             }
         },
     }
