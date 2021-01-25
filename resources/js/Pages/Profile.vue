09172cf89e2d2b0d471f6ace7b140992e3bc6862
Sam <s@sammagee.me>
2021-01-25 17:38:35 -0600

    fix footer in pen cards

--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -54,7 +54,7 @@
             >
                 <inertia-link
                     v-for="pen in pens"
-                    class="transition ease-in-out transform bg-white border-t border-b border-gray-100 duration-250 sm:border-r sm:border-l dark:border-gray-800 dark:bg-gray-900 sm:rounded-lg sm:hover:-translate-y-2 sm:focus:-translate-y-2 sm:hover:shadow sm:focus:shadow focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-gray-900"
+                    class="flex flex-col overflow-hidden transition ease-in-out transform bg-white border-t border-b border-gray-100 duration-250 sm:border-r sm:border-l dark:border-gray-800 dark:bg-gray-900 sm:rounded-lg sm:hover:-translate-y-2 sm:focus:-translate-y-2 sm:hover:shadow sm:focus:shadow focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:ring-offset-white dark:focus:ring-offset-gray-900"
                     :href="route('pen.show', pen)"
                     :key="pen.id"
                 >
@@ -68,7 +68,7 @@
                         </p>
                     </div>
 
-                    <footer class="flex items-center p-4 mt-auto space-x-2 bg-gray-100 sm:rounded-b-lg sm:py-3 sm:px-6 dark:bg-gray-800">
+                    <footer class="flex items-center p-4 mt-auto space-x-2 bg-gray-100 sm:py-3 sm:px-6 dark:bg-gray-800">
                         <svg class="w-5 h-5 text-gray-400 dark:text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg>
