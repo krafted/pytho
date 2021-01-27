ca81caaa3caf3ddb7c4edace49488e115164c343
Sam <s@sammagee.me>
2021-01-27 09:22:09 -0600

    adjust padding for tab bar and header

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -3,7 +3,7 @@
         class="fixed inset-x-0 top-0 z-30 px-4 bg-white bg-opacity-75 border-b border-gray-100 sm:px-0 pt-safe-top dark:border-gray-800 backdrop-filter-blur dark:bg-opacity-75 dark:bg-gray-900"
         :class="{ 'shadow-sm': !route().current('pen.show') }"
     >
-        <div :class="{'max-w-7xl mx-auto sm:px-4': !route().current('pen.show')}">
+        <div class="sm:px-4" :class="{'max-w-7xl mx-auto': !route().current('pen.show')}">
             <div
                 class="flex items-center justify-between w-full py-1 pl-safe-left pr-safe-right"
             >
--- resources/js/Components/TabBar.vue
+++ resources/js/Components/TabBar.vue
@@ -1,61 +1,64 @@
 <template>
-    <header class="absolute z-10 flex items-center justify-between w-full px-4 border-b border-gray-100 dark:border-gray-800">
-        <h3 class="hidden py-4 -mb-px font-mono text-xs font-semibold tracking-wide text-gray-500 uppercase border-t border-b-2 border-transparent select-none dark:text-gray-700 md:block pl-safe-left">Editor</h3>
+    <header class="absolute z-10 w-full px-4 border-b border-gray-100 dark:border-gray-800">
+        <div class="flex items-center justify-between w-full pl-safe-left pr-safe-right">
+            <h3 class="hidden py-4 -mb-px font-mono text-xs font-semibold tracking-wide text-gray-500 uppercase border-t border-b-2 border-transparent select-none dark:text-gray-700 md:block">Editor</h3>
 
-        <div class="flex items-center justify-between w-full -mb-px space-x-4 border-transparent md:hidden pl-safe-left pr-safe-right">
-            <div class="flex items-center mt-px space-x-4">
-                <button
-                    class="flex py-4 border-b-2 focus:outline-none"
-                    :class="{
-                        'border-primary-500 text-gray-900 dark:text-gray-200': !isMd && activeTab === 'editor',
-                        'border-transparent text-gray-500 dark:text-gray-700 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400': isMd || activeTab !== 'editor',
-                    }"
-                    @click="activeTab = 'editor'"
-                >
-                    <h3 class="font-mono text-xs font-semibold tracking-wide uppercase select-none">Editor</h3>
-                </button>
+            <div class="flex items-center justify-between w-full -mb-px space-x-4 border-transparent md:hidden pl-safe-left">
+                <div class="flex items-center mt-px space-x-4">
+                    <button
+                        class="flex py-4 border-b-2 focus:outline-none"
+                        :class="{
+                            'border-primary-500 text-gray-900 dark:text-gray-200': !isMd && activeTab === 'editor',
+                            'border-transparent text-gray-500 dark:text-gray-700 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400': isMd || activeTab !== 'editor',
+                        }"
+                        @click="activeTab = 'editor'"
+                    >
+                        <h3 class="font-mono text-xs font-semibold tracking-wide uppercase select-none">Editor</h3>
+                    </button>
 
-                <button
-                    v-if="!isMd"
-                    class="flex py-4 border-b-2 focus:outline-none"
-                    :class="{
-                        'border-primary-500 text-gray-900 dark:text-gray-200': activeTab === 'output',
-                        'border-transparent text-gray-500 dark:text-gray-700 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400': activeTab !== 'output',
-                    }"
-                    @click="activeTab = 'output', run()"
-                >
-                    <h3 class="font-mono text-xs font-semibold tracking-wide uppercase select-none">Output</h3>
-                </button>
+                    <button
+                        v-if="!isMd"
+                        class="flex py-4 border-b-2 focus:outline-none"
+                        :class="{
+                            'border-primary-500 text-gray-900 dark:text-gray-200': activeTab === 'output',
+                            'border-transparent text-gray-500 dark:text-gray-700 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400': activeTab !== 'output',
+                        }"
+                        @click="activeTab = 'output', run()"
+                    >
+                        <h3 class="font-mono text-xs font-semibold tracking-wide uppercase select-none">Output</h3>
+                    </button>
+                </div>
             </div>
-        </div>
 
-        <h-switch-group
-            as="div"
-            title="Auto-Run"
-        >
-            <span class="sr-only">
-                Auto-Run
-            </span>
-
-            <h-switch
-                as="button"
-                class="flex items-center justify-center p-2.5 -mt-px -mr-2 text-gray-500 border border-transparent rounded-md dark:text-gray-700 group hover:w-auto hover:bg-gray-200 dark:hover:bg-gray-1000 focus:bg-gray-200 dark:focus:bg-gray-1000 focus:border-gray-300 dark:focus:border-gray-800 hover:border-gray-300 dark:hover:border-gray-800 hover:text-gray-900 dark:hover:text-gray-400 focus:text-gray-900 dark:focus:text-gray-400 focus:outline-none focus:w-auto"
-                :model-value="preferences.autoRun"
-                v-slot="{ checked }"
-                @update:model-value="updateAutoRun"
+            <h-switch-group
+                as="div"
+                class="top-px"
+                title="Auto-Run"
             >
-                <svg
-                    class="w-5 h-5 transition-all duration-200 ease-in-out"
-                    :class="preferences.autoRun ? 'text-primary-500' : 'text-gray-500 dark:text-gray-700'"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
+                <span class="sr-only">
+                    Auto-Run
+                </span>
+
+                <h-switch
+                    as="button"
+                    class="flex items-center justify-center p-2.5 -mt-px -mr-2 text-gray-500 border border-transparent rounded-md dark:text-gray-700 group hover:w-auto hover:bg-gray-200 dark:hover:bg-gray-1000 focus:bg-gray-200 dark:focus:bg-gray-1000 focus:border-gray-300 dark:focus:border-gray-800 hover:border-gray-300 dark:hover:border-gray-800 hover:text-gray-900 dark:hover:text-gray-400 focus:text-gray-900 dark:focus:text-gray-400 focus:outline-none focus:w-auto"
+                    :model-value="preferences.autoRun"
+                    v-slot="{ checked }"
+                    @update:model-value="updateAutoRun"
                 >
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                </svg>
-            </h-switch>
-        </h-switch-group>
+                    <svg
+                        class="w-5 h-5 transition-all duration-200 ease-in-out"
+                        :class="preferences.autoRun ? 'text-primary-500' : 'text-gray-500 dark:text-gray-700'"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                    </svg>
+                </h-switch>
+            </h-switch-group>
+        </div>
     </header>
 </template>
 
