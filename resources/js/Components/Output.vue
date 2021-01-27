f8ecaaaf20ce426b2e5b2c73499872c5f6efc335
Sam <s@sammagee.me>
2021-01-27 09:32:04 -0600

    fix output window padding

--- resources/js/Components/Output.vue
+++ resources/js/Components/Output.vue
@@ -17,7 +17,7 @@
         <transition leave-active-class="duration-200">
             <div
                 v-show="show"
-                class="absolute inset-0 z-10 flex flex-1 overflow-hidden"
+                class="absolute inset-0 z-10 flex flex-1 overflow-hidden pr-safe-right"
                 :class="{ 'pl-safe-left': preferences.layout === 'horizontal' }"
             >
                 <transition
@@ -30,7 +30,7 @@
                 >
                     <div
                         v-show="show"
-                        class="absolute inset-0 flex items-center justify-center flex-1 p-12 overflow-auto transition-opacity bg-gray-100 bg-opacity-25 dark:bg-gray-1000 dark:bg-opacity-25 backdrop-filter-blur pr-safe-right focus:outline-none"
+                        class="absolute inset-0 flex items-center justify-center flex-1 p-12 overflow-auto transition-opacity bg-gray-100 bg-opacity-25 dark:bg-gray-1000 dark:bg-opacity-25 backdrop-filter-blur focus:outline-none"
                     >
                         <transition
                             enter-active-class="duration-300 ease-out delay-300"
