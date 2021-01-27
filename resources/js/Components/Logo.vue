8527bca9e6aa328d120d16ce9ac24bc969cd6e3e
Sam <s@sammagee.me>
2021-01-27 13:07:42 -0600

    fix settings layout header

--- resources/js/Components/Logo.vue
+++ resources/js/Components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
     <inertia-link
-        class="flex items-center justify-center font-mono font-semibold text-gray-600 bg-white bg-opacity-75 border border-white rounded-md select-none backdrop-filter-blur dark:bg-opacity-75 dark:text-gray-400 dark:bg-gray-900 dark:border-gray-900 hover:bg-gray-200 dark:hover:bg-gray-1000 focus:bg-gray-200 dark:focus:bg-gray-1000 focus:border-gray-300 dark:focus:border-gray-600 hover:border-gray-300 dark:hover:border-gray-600 hover:text-gray-900 dark:hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-200 dark:focus:ring-offset-gray-900 focus:ring-primary-500"
+        class="flex items-center justify-center font-mono font-semibold text-gray-600 border border-transparent rounded-md select-none dark:text-gray-400 dark:border-transparent hover:bg-gray-200 dark:hover:bg-gray-1000 focus:bg-gray-200 dark:focus:bg-gray-1000 focus:border-gray-300 dark:focus:border-gray-600 hover:border-gray-300 dark:hover:border-gray-600 hover:text-gray-900 dark:hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-200 dark:focus:ring-offset-gray-900 focus:ring-primary-500"
         href="/"
     >
         Py
--- resources/js/Layouts/Settings.vue
+++ resources/js/Layouts/Settings.vue
@@ -1,68 +1,65 @@
 <template>
     <app-layout>
-        <div class="w-full mx-auto mt-16 sm:px-4 max-w-7xl">
-            <h1 class="px-4 text-4xl font-semibold leading-tight text-gray-900 sm:px-0 dark:text-gray-100">
-                Settings
-            </h1>
-        </div>
+        <div class="w-full py-6 mx-auto mt-6 sm:px-4 max-w-7xl">
+            <div class="pl-safe-left pr-safe-right">
+                <h1 class="px-4 text-4xl font-semibold leading-tight text-gray-900 sm:px-0 dark:text-gray-100">
+                    Settings
+                </h1>
 
-        <header class="sticky top-12.5 z-10 bg-white dark:bg-gray-900 mt-6 border-b border-gray-100 dark:border-gray-800 backdrop-filter-blur bg-opacity-75 dark:bg-opacity-75">
-            <nav class="flex items-center w-full px-4 mx-auto -mb-px space-x-4 max-w-7xl sm:px-4">
-                <inertia-link
-                    v-if="page.props.value.jetstream.canUpdateProfileInformation"
-                    class="flex items-center py-4 border-b-2 focus:outline-none"
-                    :class="[
-                        route().current('settings.profile.show')
-                            ? 'border-primary-500 text-gray-900 dark:text-gray-200'
-                            : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
-                    ]"
-                    :href="route('settings.profile.show')"
-                >
-                    Profile
-                </inertia-link>
+                <header class="px-4 mt-6 border-b border-gray-100 sm:px-0 dark:border-gray-800">
+                    <nav class="flex items-center -mb-px space-x-4">
+                        <inertia-link
+                            v-if="page.props.value.jetstream.canUpdateProfileInformation"
+                            class="flex items-center py-4 border-b-2 focus:outline-none"
+                            :class="[
+                                route().current('settings.profile.show')
+                                    ? 'border-primary-500 text-gray-900 dark:text-gray-200'
+                                    : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
+                            ]"
+                            :href="route('settings.profile.show')"
+                        >
+                            Profile
+                        </inertia-link>
 
-                <inertia-link
-                    class="flex items-center py-4 border-b-2 focus:outline-none"
-                    :class="[
-                        route().current('settings.security.show')
-                            ? 'border-primary-500 text-gray-900 dark:text-gray-200'
-                            : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
-                    ]"
-                    :href="route('settings.security.show')"
-                >
-                    Security
-                </inertia-link>
+                        <inertia-link
+                            class="flex items-center py-4 border-b-2 focus:outline-none"
+                            :class="[
+                                route().current('settings.security.show')
+                                    ? 'border-primary-500 text-gray-900 dark:text-gray-200'
+                                    : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
+                            ]"
+                            :href="route('settings.security.show')"
+                        >
+                            Security
+                        </inertia-link>
 
-                <inertia-link
-                    v-if="page.props.value.socialstream.show"
-                    class="flex items-center py-4 border-b-2 focus:outline-none"
-                    :class="[
-                        route().current('settings.social.show')
-                            ? 'border-primary-500 text-gray-900 dark:text-gray-200'
-                            : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
-                    ]"
-                    :href="route('settings.social.show')"
-                >
-                    Social
-                </inertia-link>
+                        <inertia-link
+                            v-if="page.props.value.socialstream.show"
+                            class="flex items-center py-4 border-b-2 focus:outline-none"
+                            :class="[
+                                route().current('settings.social.show')
+                                    ? 'border-primary-500 text-gray-900 dark:text-gray-200'
+                                    : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
+                            ]"
+                            :href="route('settings.social.show')"
+                        >
+                            Social
+                        </inertia-link>
 
-                <inertia-link
-                    v-if="page.props.value.socialstream.hasPassword"
-                    class="flex items-center py-4 border-b-2 focus:outline-none"
-                    :class="[
-                        route().current('settings.advanced.show')
-                            ? 'border-primary-500 text-gray-900 dark:text-gray-200'
-                            : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
-                    ]"
-                    :href="route('settings.advanced.show')"
-                >
-                    Advanced
-                </inertia-link>
-            </nav>
-        </header>
-
-        <div class="w-full py-6 mx-auto sm:px-4 max-w-7xl">
-            <div class="mt-6 pl-safe-left pr-safe-right">
+                        <inertia-link
+                            v-if="page.props.value.socialstream.hasPassword"
+                            class="flex items-center py-4 border-b-2 focus:outline-none"
+                            :class="[
+                                route().current('settings.advanced.show')
+                                    ? 'border-primary-500 text-gray-900 dark:text-gray-200'
+                                    : 'border-transparent text-gray-500 hover:text-gray-600 dark:hover:text-gray-400 focus:text-gray-600 dark:focus:text-gray-400'
+                            ]"
+                            :href="route('settings.advanced.show')"
+                        >
+                            Advanced
+                        </inertia-link>
+                    </nav>
+                </header>
                 <div class="mt-6">
                     <slot />
                 </div>
