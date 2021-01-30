e3a5cce17518605ba0180bda77b4fb5d00091840
Sam <s@sammagee.me>
2021-01-30 00:21:56 -0600

    add safe area padding to footer

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -201,7 +201,7 @@
         </div>
 
         <div class="px-16 pt-6 pb-24 mt-40">
-            <div class="w-full max-w-5xl mx-auto">
+            <div class="w-full max-w-5xl mx-auto pl-safe-left pr-safe-right">
                 <div class="grid grid-cols-3 gap-3">
                     <p class="font-medium text-gray-500">
                         &copy; <inertia-link class="hover:underline focus:ring-2 focus:outline-none focus:ring-primary-500" :href="route('pen.show')">Py</inertia-link>
@@ -221,7 +221,7 @@
                     </p>
                 </div>
 
-                <p class="flex items-center justify-center mt-3 text-gray-500 md:hidden">
+                <p class="flex items-center justify-center mt-6 text-gray-500 md:hidden">
                     Made with&nbsp;
                     <svg class="flex-shrink-0 w-4 h-4 text-primary-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
